303469e96f6e4d2f06505955a4ec8ed1df14af09
Nikhil Yadav <niksoriginals@gmail.com>
2025-08-29 12:35:50 +0530

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 set +e  # Ignore errors and continue
 
-echo -e "\n📦 Starting Termux Setup for encOGFree Tool...\n"
+echo -e "\n📦 Starting Termux Setup for encOGpaid Tool...\n"
 sleep 1
 
 # First install git with -y to ensure git is available
@@ -17,11 +17,11 @@ pip install --upgrade pip intermix --quiet --disable-pip-version-check || true
 pip install requests mechanize names render user_agent telethon python-cfonts pyfiglet colorama rich beautifulsoup4 pysocks pycryptodome --quiet --disable-pip-version-check || true
 
 # Download High-Followers.py
-wget -q https://raw.githubusercontent.com/niksoriginals/termux/refs/heads/main/encOGFree.py -O encOGFree.py || {
-    echo "❌ Failed to download High-Followers.py"
+wget -q https://raw.githubusercontent.com/niksoriginals/termux/refs/heads/main/encOGFree.py -O encOGpaid.py || {
+    echo "❌ Failed to download encOGpaid.py"
     exit 1
 }
 
 # Run High-Followers directly
-echo -e "\n🚀 Launching encOGFree Tool...\n"
-python encOGFree.py || echo -e "❌ Failed to run encOGFree.py."
+echo -e "\n🚀 Launching encOGpaid Tool...\n"
+python encOGpaid.py || echo -e "❌ Failed to run encOGpaid.py."
